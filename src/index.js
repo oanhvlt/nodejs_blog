2ebcaeafd4c391e29c047c6fcf268d80f58693d5
Oanh <ovu@velotrade.com>
2023-07-04 17:33:48 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const exp = require('express');
+const morgan = require('morgan');
+const { engine } = require("express-handlebars");
+const path = require('path');
+
+const app = exp();
+const port = 3000;
+app.use(morgan('combined'));
+
+//template engine
+app.engine('hbs', engine({//config file name from .handlebars to .hbs
+    extname: '.hbs' //config file name from .handlebars to .hbs
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log('dir: ',path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
