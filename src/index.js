523f0d492e03d8203a0f6107c5d6a9aaa2382c48
Oanh <ovu@velotrade.com>
2023-07-05 14:53:50 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const path = require('path');
 
 const app = exp();
 const port = 3000;
+
+//HTTP logger
 app.use(morgan('combined'));
+
+//use static file: show logo by route http://localhost:3000/img/logo.png
+app.use(exp.static(path.join(__dirname,'public')));
 
 //template engine
 app.engine('hbs', engine({//config file name from .handlebars to .hbs
