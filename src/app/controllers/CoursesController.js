b2076f1ac6ee4865d51ec26ed0871703dfb97659
Oanh <ovu@velotrade.com>
2023-07-25 17:08:21 +0700

    course detail page

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -1,8 +1,21 @@
+const courseModel = require('../models/Course');
+const {mongooseToObj} = require('../../until/mongoose');
 
 class CourseController {
     //[GET]  /courses/:slug
-    show(req, res){
-        res.send('Courses details')
+    show(req, res, next){
+        //res.send(`<h2>Courses details ${req.params.slug}</h2>`); //follow by '/:slug' on coursesRouter.js
+        
+        /** Access model: use Promise **/
+        //req.params.slug: follow by '/:slug' on coursesRouter.js
+        courseModel.findOne({slug: req.params.slug}) //findOne({field in DB: value reference})
+                    .then(courseF8 => {
+                        //res.json(course)
+                        res.render('courses/show', {
+                            course: mongooseToObj(courseF8)//trả dữ liệu cho view: courses
+                        }) 
+                    })
+                    .catch(next);
     }
 }
 
--- src/until/mongoose.js
+++ src/until/mongoose.js
@@ -3,8 +3,8 @@ module.exports = {
     multipleMongooseToObj: function (courseF8Array){
         return courseF8Array.map(course => course.toObject());
     },
-    mongooseToObj: function (course){
-        return course ? course.toObject() : course;
+    mongooseToObj: function (courseF8){
+        return courseF8 ? courseF8.toObject() : courseF8;
     },
     
 };
